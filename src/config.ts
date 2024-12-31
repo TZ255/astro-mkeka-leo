47df52da369349cdf8731e26b2025b988c35f68f
gshemdoe <georgehmariki@gmail.com>
2024-12-31 19:10:34 +0300

    updated

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@
 
 export const SITE_TITLE = 'Mikeka ya Uhakika | Pata Mikeka ya Bure Kila Siku';
 export const SITE_DESCRIPTION = 'Tanzania football tips & predictions. Pata mikeka ya uhakika kila siku. Mikeka ya bure, mikeka ya jumamosi na jumapili inapatikana hapa. Karibu nyumba ya ushindi';
-export const GENERATE_SLUG_FROM_TITLE = true
+export const GENERATE_SLUG_FROM_TITLE = false
 export const TRANSITION_API = true
